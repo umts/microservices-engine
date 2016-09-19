075b3ba101bcb5d73d472d464b4c684355367582
Dylan Bowers <djbowers@umass.edu>
2016-09-19 17:19:13 -0400

    endpoint for receiving data framework

--- app/controllers/microservices_engine/v1/data_controller.rb
+++ app/controllers/microservices_engine/v1/data_controller.rb
@@ -6,6 +6,21 @@ module MicroservicesEngine
       def update
         # TO-DO
         # . . .
+
+        token = params.require(:token)
+        # TO-DO
+        #
+        # Add logic to verify token, perhaps send a request to the router
+        # to ask if the token is valid?
+
+        data = params.require(:content)
+        # TO-DO
+        #
+        # Generate connection objects / update existing connection objects
+        # based on the contents of the request.
+        #
+        # Requires knowledge of how the router will send data to engines
+        # so this will be delayed
       end
     end
   end
