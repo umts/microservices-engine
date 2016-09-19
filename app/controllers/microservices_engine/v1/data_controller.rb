e7e08668e4a0c402c5c3a72163724369d15ccb89
Dylan Bowers <djbowers@umass.edu>
2016-09-19 16:50:05 -0400

    remove unnecessary stuff

--- app/controllers/microservices_engine/v1/data_controller.rb
+++ app/controllers/microservices_engine/v1/data_controller.rb
@@ -3,45 +3,6 @@ require_dependency "microservices_engine/application_controller"
 module MicroservicesEngine
   module V1
     class DataController < ApplicationController
-
-      def index
-        # REST Endpoint
-        # Used to retrieve data from the local application
-        # Usage: http://(url)/data?models={MODELS}
-        #        MODELS: A set of model names to request data of separated
-        #                by commas.
-        # Example: http://larrysenior.com/data?models=ChewingGum,LarrysKid
-
-        objs = params.require(:models) #.permit(:attributes)
-
-        models = objs.split(',')
-        # if objs['attributes']
-        #   attrs = objs['attributes'].split(',')
-        # end
-
-        valid_model_names = MicroservicesEngine.config.endpoint_models.map{|mdl| mdl.name}
-
-        if models.all? { |param| param.in? valid_model_names }
-          content = {}
-          models.each do |mdl|
-            content[mdl] ||= []
-
-            mdl.constantize.all.each do |m_obj|
-              content[mdl] << m_obj.attributes
-            end
-          end
-          render json: {
-            'response': 200,
-            'content': content
-          }
-        else
-          render json: {
-            'response': 404,
-            'content': 'One or more of the requested models are not available here'
-          }
-        end
-      end
-
       def update
         # TO-DO
         # . . .
--- db/migrate/20160915193617_create_microservices_engine_configurations.rb
+++
@@ -1,8 +0,0 @@
-class CreateMicroservicesEngineConfigurations < ActiveRecord::Migration
-  def change
-    create_table :microservices_engine_configurations do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
