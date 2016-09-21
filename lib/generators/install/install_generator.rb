d35251518b367ee7f07285699ec9ac0d76e8ecfd
Dylan Bowers <djbowers@umass.edu>
2016-09-21 17:03:51 -0400

    NOW the generator is modified

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -1,11 +1,9 @@
 class InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
-  argument :objects, :type => :string, default: "b"
 
   def setup_initializer
-    # TO-DO
-    
-    # Replace the content of this method with the logic for the new
-    # install initializer as depicted on the GitHub issue #5
+    template "mse_router_info.yml", "config/mse_router_info.yml"
+    copy_file "microservices_engine.rb",
+      "config/initializers/microservices_engine.rb"
   end
 end
