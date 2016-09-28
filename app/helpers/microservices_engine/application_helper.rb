b482d94d6a94c9857cb3816460d54ef81f437d4d
Dylan Bowers <djbowers@umass.edu>
2016-09-28 16:43:43 -0400

    removed unnecessary directories

--- app/helpers/microservices_engine/application_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-module MicroservicesEngine
-  module ApplicationHelper
-  end
-end
--- app/helpers/microservices_engine/data_helper.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-module MicroservicesEngine
-  module DataHelper
-  end
-end
