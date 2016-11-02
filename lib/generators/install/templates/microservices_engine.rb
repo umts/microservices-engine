dd0f99903d4401230077e091f4a03aa013c9d07e
Dylan Bowers <djbowers@umass.edu>
2016-11-02 16:44:49 -0400

    config tokens and some docs

--- lib/generators/install/templates/microservices_engine.rb
+++ lib/generators/install/templates/microservices_engine.rb
@@ -8,20 +8,22 @@ end
 
 config_data = YAML.load_file(config_file)
 
-if [config_data['name'], config_data['uri']].any?(&:blank?)
+if [config_data['name'], config_data['uri'], config_data['security_token']].any?(&:blank?)
   raise '[MSE] > Please fill out config/mse_router_info.yml'
 end
 
 # Make sure the URI ends with a / character
-uri = config['router_uri']
+router_uri = config['router_uri']
+
+uri = config_data['uri']
 uri += '/' if uri[-1] != '/'
 
 res = Net::HTTP.post_form(
-  URI(uri),
-  'ph_name': config_data['name'],
-  'ph_content': config_data['accessible_models']
+  URI(router_uri),
+  'name': config_data['name'],
+  'url': uri
+  'models': config_data['accessible_models'],
+  'security_token': config_data['security_token']
 )
 
 raise '[MSE] > The router API response was invalid' if res.code != '200'
-# TODO
-# Verify that the contents of the response are what we expect to happen
