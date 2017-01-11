c7bf93991b0e6db204121463c5b3bdee22c6d81b
anbranin <keichelman@gmail.com>
2017-01-11 15:05:03 -0500

    yaml keys must be strings

--- lib/generators/install/templates/microservices_engine.rb
+++ lib/generators/install/templates/microservices_engine.rb
@@ -12,10 +12,10 @@ end
 
 config_data = YAML.load_file(config_file)
 
-service_name = config_data.fetch :name, &REPORT_MISSING_CONFIG_KEY
-service_url = config_data.fetch :uri, &REPORT_MISSING_CONFIG_KEY
-router_url = config_data.fetch :router_uri, &REPORT_MISSING_CONFIG_KEY
-security_token = config_data.fetch :security_token, &REPORT_MISSING_CONFIG_KEY
+service_name = config_data.fetch 'name', &REPORT_MISSING_CONFIG_KEY
+service_url = config_data.fetch 'uri', &REPORT_MISSING_CONFIG_KEY
+router_url = config_data.fetch 'router_uri', &REPORT_MISSING_CONFIG_KEY
+security_token = config_data.fetch 'security_token', &REPORT_MISSING_CONFIG_KEY
 
 res = Net::HTTP.post_form(
   URI.parse(router_url),
