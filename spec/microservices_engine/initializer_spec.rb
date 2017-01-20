33be95c17aa1b123d88c148dcef0a172c7408cc4
David Faulkenberry <david.faulkenberry@gmail.com>
2017-01-20 10:32:19 -0500

    Finish tests for report_missing_config_key

--- spec/microservices_engine/initializer_spec.rb
+++ spec/microservices_engine/initializer_spec.rb
@@ -3,9 +3,16 @@ require 'generators/install/templates/microservices_engine'
 
 describe MicroservicesEngine::Initializer do
   describe 'report_missing_config_key' do
-    let(:call) { described_class.report_missing_config_key :key }
+    let(:call) { described_class.report_missing_config_key :bananas }
     it 'raises an ArgumentError' do
       expect{ call }.to raise_error ArgumentError
+    end
+    it 'includes the missing key in the exception' do
+      expect{ call }.to raise_error ArgumentError, /bananas/
+    end
+    it 'includes the location of the config file in the exception' do
+      stub_const 'MicroservicesEngine::Initializer::CONFIG_FILE', 'apples'
+      expect{ call }.to raise_error ArgumentError, /apples/
     end
   end
 end
