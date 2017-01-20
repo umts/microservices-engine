c5349ad65b7e9965e220ab99fa216749bec3f71c
David Faulkenberry <david.faulkenberry@gmail.com>
2017-01-20 10:36:04 -0500

    Test check_for_config_file!

--- spec/microservices_engine/initializer_spec.rb
+++ spec/microservices_engine/initializer_spec.rb
@@ -2,6 +2,23 @@ require 'spec_helper'
 require 'generators/install/templates/microservices_engine'
 
 describe MicroservicesEngine::Initializer do
+  describe 'check_for_config_file!' do
+    let(:filepath) { 'some_filepath' }
+    let(:call) { described_class.check_for_config_file! filepath }
+    it 'looks for a file at the given path' do
+      expect(File).to receive(:file?).with(filepath).and_return true
+      call
+    end
+    it "raises an IOError if it can't find one" do
+      expect(File).to receive(:file?).with(filepath).and_return false
+      expect { call }.to raise_error IOError
+    end
+    it 'includes the filepath in the exception' do
+      expect(File).to receive(:file?).with(filepath).and_return false
+      expect { call }.to raise_error IOError, /#{filepath}/
+    end
+  end
+
   describe 'report_missing_config_key' do
     let(:call) { described_class.report_missing_config_key :bananas }
     it 'raises an ArgumentError' do
