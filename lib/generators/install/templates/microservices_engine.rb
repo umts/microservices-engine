a1ad8ee66ff6aab7a9bada1f3e9e3cd18f9693e1
David Faulkenberry <david.faulkenberry@gmail.com>
2017-01-20 09:21:29 -0500

    Organize into a class with sub-methods

--- lib/generators/install/templates/microservices_engine.rb
+++ lib/generators/install/templates/microservices_engine.rb
@@ -1,46 +1,67 @@
 # frozen_string_literal: true
 require 'net/http'
-CONFIG_FILE = 'config/mse_router_info.yml'
 
-unless ENV['DISABLE_ROUTER_CHECKIN']
+module MicroservicesEngine
+  class Initializer
+    CONFIG_FILE = 'config/mse_router_info.yml'
 
-  report_missing_config_key = lambda do |key|
-    raise ArgumentError, "Missing key #{key} in #{CONFIG_FILE}"
-  end
+    def initialize!
+      unless ENV.key? 'DISABLE_ROUTER_CHECKIN'
+        check_for_config_file! CONFIG_FILE
+        config_data = YAML.load_file CONFIG_FILE
+        response = check_in_with_router using: config_data
+        response_data = JSON.parse response.body
+        update_connections_list with: response_data
+        raise 'The router API response was invalid' if response.code != '200'
+      end
+    end
 
-  unless File.file?(CONFIG_FILE)
-    raise IOError, 'No router configuration YAML file found'
-  end
+    def check_for_config_file!(filename)
+      unless File.file? filename
+        raise IOError, "No router configuration file found at #{filename}"
+      end
+    end
 
-  config_data = YAML.load_file(CONFIG_FILE)
+    def check_in_with_router(options = {})
+      if options.key? :using
+        config_data = options[:using]
+        service_name = config_data.fetch 'name', &:report_missing_config_key
+        service_url = config_data.fetch 'uri', &:report_missing_config_key
+        router_url = config_data.fetch 'router_uri', &:report_missing_config_key
+        security_token = config_data.fetch 'security_token', &:report_missing_config_key
 
-  service_name = config_data.fetch 'name', &report_missing_config_key
-  service_url = config_data.fetch 'uri', &report_missing_config_key
-  router_url = config_data.fetch 'router_uri', &report_missing_config_key
-  security_token = config_data.fetch 'security_token', &report_missing_config_key
+        router_url = router_url + '/services/register'
 
-  router_url = router_url + '/services/register'
+        response = Net::HTTP.post_form(
+          URI.parse(router_url),
+          name: service_name,
+          url: service_url,
+          models: config_data['accessible_models'],
+          security_token: security_token
+        )
+      end
+    end
 
-  response = Net::HTTP.post_form(
-    URI.parse(router_url),
-    name: service_name,
-    url: service_url,
-    models: config_data['accessible_models'],
-    security_token: security_token
-  )
+    def report_missing_config_key(key)
+      raise ArgumentError, "Missing key #{key} in #{CONFIG_FILE}"
+    end
 
-  response_data = JSON.parse(response.body)
-  MicroservicesEngine::Connection.destroy_all
-  response_data.each do |service|
-    if service['models'].present?
-      url = service['url']
-      service['models'].each do |model|
-        object = model['name']
-        MicroservicesEngine::Connection.create!  url: url, object: object
+    def update_connections_list(options = {})
+      if options.key? :with
+        response_data = options[:with]
+        MicroservicesEngine::Connection.destroy_all
+        response_data.each do |service|
+          if service['models'].present?
+            url = service['url']
+            service['models'].each do |model|
+              object = model['name']
+              MicroservicesEngine::Connection.create!  url: url, object: object
+            end
+          end
+        end
       end
     end
   end
-
-  raise 'The router API response was invalid' if res.code != '200'
-
 end
+
+MicroservicesEngine::Initializer.initialize!
