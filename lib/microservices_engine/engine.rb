5d6f92207dc2bb954a1e9c8401f07c1d33531fa9
anbranin <keichelman@gmail.com>
2017-01-26 17:18:09 -0500

    what-about-this

--- lib/microservices_engine/engine.rb
+++ lib/microservices_engine/engine.rb
@@ -8,6 +8,9 @@ module MicroservicesEngine
     end
 
     # do something different based on container app's rails version?
+    # this will only work on rails 4, not 3. How to obtain from app
+    # in initializer?
+    # Also, do this or just debug the other way?
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
