aeb5282373215513edc8683c55616f77f49461c6
anbranin <keichelman@gmail.com>
2017-01-26 16:59:19 -0500

    this only works for 4, make it work for 3

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
+# require 'rails/generators/active_record'
 class InstallGenerator < Rails::Generators::Base
+  # include ActiveRecord::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def setup_initializer
     template 'mse_router_info.yml', 'config/mse_router_info.yml'
     copy_file 'microservices_engine.rb',
               'config/initializers/microservices_engine.rb'
-    migration_template 'create_microservices_engine_connections.rb',
-                       'db/migrate/create_microservices_engine_connections',
-                       migration_version: migration_version
+   # migration_template 'create_microservices_engine_connections.rb',
+   #                    'db/migrate/create_microservices_engine_connections',
+   #                    migration_version: migration_version
   end
 
-  # idea for this is from the 'devise' gem by plataformatec
+  # Nah
   def migration_version
     if Rails::VERSION::MAJOR >= 5
       "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
--- lib/microservices_engine/engine.rb
+++ lib/microservices_engine/engine.rb
@@ -6,5 +6,14 @@ module MicroservicesEngine
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    # do something different based on container app's rails version?
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
   end
 end
