81bb2dc5c43cadcc815935673edb79d08f64746c
anbranin <keichelman@gmail.com>
2017-01-27 14:44:37 -0500

    dummy app used to test migration template, works

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916212601) do
+ActiveRecord::Schema.define(version: 20170127193951) do
 
   create_table "field_trips", force: :cascade do |t|
     t.string   "name"
