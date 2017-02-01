8494ea0af25512f7162b5b86e8bd65cf9f696ccc
Dylan Bowers <djbowers@umass.edu>
2017-02-01 13:41:45 -0500

    will this fix travis?

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 require 'rails/generators/active_record'
-class InstallGenerator < ActiveRecord::Generators::Base 
+class InstallGenerator < ActiveRecord::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def setup_initializer
     template 'mse_router_info.yml', 'config/mse_router_info.yml'
     copy_file 'microservices_engine.rb',
               'config/initializers/microservices_engine.rb'
-    migration_template 'create_microservices_engine_connections.rb',
-                       'db/migrate/create_microservices_engine_connections.rb',
-                       migration_version: migration_version
+    # migration_template 'create_microservices_engine_connections.rb',
+    #                    'db/migrate/create_microservices_engine_connections.rb',
+    #                    migration_version: migration_version
   end
 
   # idea for this is from the 'devise' gem by plataformatec
