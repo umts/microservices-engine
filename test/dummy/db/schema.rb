b30fb2b2879b948f51b7fab18f4dcf7d0df7ef73
anbranin <keichelman@gmail.com>
2017-02-02 16:39:19 -0500

    cannot have two tables with identical names here

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,18 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130215723) do
+ActiveRecord::Schema.define(version: 20160916212601) do
 
   create_table "field_trips", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "microservices_engine_connections", force: :cascade do |t|
-    t.string "name"
-    t.string "object"
-    t.string "url"
   end
 
   create_table "test_models", force: :cascade do |t|
