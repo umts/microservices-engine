2ff31ad920dcf1468c773d6b53ff034d0e1c0340
anbranin <keichelman@gmail.com>
2017-02-02 12:30:26 -0500

    tested migrations on dummy app

--- test/dummy/db/migrate/20170130215723_create_microservices_engine_connection.rb
+++ test/dummy/db/migrate/20170130215723_create_microservices_engine_connection.rb
@@ -1,11 +1,9 @@
-class CreateMicroservicesEngineConnections < ActiveRecord::Migration[5.0]
+class CreateMicroservicesEngineConnection < ActiveRecord::Migration[5.0]
   def change
     create_table :microservices_engine_connections do |t|
       t.string :name
       t.string :object
       t.string :url
-
-      t.timestamps null: false
     end
   end
 end
