df9fe7fdf45bc97afe4f78da28e9a5363524ef4c
anbranin <keichelman@gmail.com>
2017-02-14 11:33:47 -0500

    maybe the migrate and create need to be on separate lines

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,5 +3,6 @@
 if [ "$RAILS_VERSION" = "5" ]; then
   bundle exec rails db:create db:migrate
 else
-  bundle exec rake db:create db:migrate
+  bundle exec rake db:create
+  bundle exec rake db:migrate
 fi
