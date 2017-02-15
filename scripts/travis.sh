f26ebb8a19bbc0dc990de664a1bd1025edd62b59
anbranin <keichelman@gmail.com>
2017-02-15 13:31:00 -0500

    try evaluating dif gemfile depending on ENV

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 if [ "$RAILS_VERSION" = "5" ]; then
-  bundle install --no-deployment --gemfile=gemfiles/rails_5.gemfile
+  #bundle install --no-deployment --gemfile=gemfiles/rails_5.gemfile
   bundle exec rails db:create db:migrate
 elif [ "$RAILS_VERSION" = "4" ]; then
-  bundle install --no-deployment --gemfile=gemfiles/rails_4.gemfile
+  #bundle install --no-deployment --gemfile=gemfiles/rails_4.gemfile
   bundle exec rake db:create db:migrate
 elif [ "$RAILS_VERSION" = "3" ]; then
-  bundle install --no-deployment --gemfile=gemfiles/rails_3.gemfile
+  #bundle install --no-deployment --gemfile=gemfiles/rails_3.gemfile
   bundle exec rake db:create db:migrate
 else
-  bundle install --no-deployment
+  #bundle install --no-deployment
   bundle exec rails db:create db:migrate
 fi
