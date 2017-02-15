296254f6fd5c818894bce980278889c6e3d370ac
anbranin <keichelman@gmail.com>
2017-02-15 12:51:15 -0500

    modify permissions on travis script.
    remove unused scripts

--- scripts/travis.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-if ENV['RAILS_VERSION'] == '5'
-  system( "RAILS_ENV=test bundle exec rails db:create db:migrate" )
-else
-  system( "RAILS_ENV=test bundle exec rake db:create" )
-  system( "RAILS_ENV=test bundle exec rake db:migrate" )
-end
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,6 +3,5 @@
 if [ "$RAILS_VERSION" = "5" ]; then
   bundle exec rails db:create db:migrate
 else
-  bundle exec rake db:create
-  bundle exec rake db:migrate
+  bundle exec rake db:create db:migrate
 fi
