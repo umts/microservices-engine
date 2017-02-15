54fc5ed33d1e3bb1a3bd446c9d1a93e3fbf09629
anbranin <keichelman@gmail.com>
2017-02-15 13:12:28 -0500

    change script to bundle different gemfiles

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 if [ "$RAILS_VERSION" = "5" ]; then
+  bundle install --gemfile=gemfiles/rails_5.gemfile
   bundle exec rails db:create db:migrate
-else
+elif [ "$RAILS_VERSION" = "4" ]; then
+  bundle install --gemfile=gemfiles/rails_4.gemfile
   bundle exec rake db:create db:migrate
+elif [ "$RAILS_VERSION" = "3" ]; then
+  bundle install --gemfile=gemfiles/rails_3.gemfile
+  bundle exec rake db:create db:migrate
+else
+  bundle install
+  bundle exec rails db:create db:migrate
 fi
