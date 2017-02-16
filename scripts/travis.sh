92c3e4b36f0c04248ca0f76c5fcd5e85bc12e359
anbranin <keichelman@gmail.com>
2017-02-15 21:38:57 -0500

    I think these need to be on separate lines

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,9 +3,11 @@
 if [ "$RAILS_VERSION" = "5" ]; then
   bundle exec rails db:create db:migrate
 elif [ "$RAILS_VERSION" = "4" ]; then
-  bundle exec rake db:create db:migrate
+  bundle exec rake db:create
+  bundle exec rake db:migrate
 elif [ "$RAILS_VERSION" = "3" ]; then
-  bundle exec rake db:create db:migrate
+  bundle exec rake db:create
+  bundle exec rake db:migrate
 else
   bundle exec rails db:create db:migrate
 fi
