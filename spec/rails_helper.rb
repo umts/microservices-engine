4a113fe8a225987d01b4d12a34c3752ba7f6973f
anbranin <keichelman@gmail.com>
2017-02-27 14:31:24 -0500

    specs change a bit if rails 3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
-require 'spec_helper'
-require File.expand_path('../../test/dummy/config/environment', __FILE__)
+
+require File.expand_path('../../test/rails3dummy/config/environment', __FILE__)
+# if rails env = this expand path of whatever
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
 require 'webmock/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -26,7 +28,9 @@ require 'webmock/rspec'
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+#
+# I do not know what to do if it's rails 3 tho. rake db:test:prepare should
+# handle it, but.
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
